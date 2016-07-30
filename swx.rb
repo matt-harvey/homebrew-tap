9030c5b113a39dc9e60faffe618d7a5a93ed398b
Matthew Harvey <software@matthewharvey.net>
2016-07-30 10:03:43 +0930

    Fix some issues with swx formula uncovered by brew audit.

--- swx.rb
+++ swx.rb
@@ -1,12 +1,7 @@
-# Documentation: https://github.com/Homebrew/brew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Swx < Formula
-  desc "Command line time tracking application"
+  desc "command-line time tracking application"
   homepage "https://github.com/matt-harvey/swx/"
   url "https://github.com/matt-harvey/swx/archive/v0.11.0.tar.gz"
-  version "0.11.0"
   sha256 "16fe84b6488c0217b1915f191efba7479773ca78021d733ae0fec4a351f3e581"
 
   depends_on "cmake" => :build
@@ -17,15 +12,6 @@ class Swx < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test swx`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system bin/"swx" --help
+    system bin/"swx", "--help"
   end
 end
