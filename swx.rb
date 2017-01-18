e7f2cc4a0d02d14de3c67d06f19437f1a5f0418a
Matthew Harvey <software@matthewharvey.net>
2017-01-18 21:16:11 +1100

    swx now at 1.0.1.

--- swx.rb
+++ swx.rb
@@ -1,8 +1,8 @@
 class Swx < Formula
   desc "command-line time tracking application"
   homepage "https://github.com/matt-harvey/swx/"
-  url "https://github.com/matt-harvey/swx/archive/v1.0.0.tar.gz"
-  sha256 "549ecfd8898c474d87249141004a42e3470e74c574ef6221b03541f463ad9cbe"
+  url "https://github.com/matt-harvey/swx/archive/v1.0.1.tar.gz"
+  sha256 "a4a33d95467eb000642be62af90485a318de01f58654e56e9d2b201ad1c1baf8"
 
   depends_on "cmake" => :build
 
